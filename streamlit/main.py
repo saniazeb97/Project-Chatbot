97c7f71e2a5f6428bad72dbddaaf96570a1e62b7
Sania Zeb <149472703+saniazeb97@users.noreply.github.com>
2025-01-26 16:20:22 +0500

    Update main.py

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from .api_client import upload_pdf_to_backend
-from .ui_helpers import display_chat_history, ask_question
+from api_client import upload_pdf_to_backend
+from ui_helpers import display_chat_history, ask_question
 
 st.title("CHAT WITH PDF")
 uploaded_file = st.sidebar.file_uploader("Upload a PDF", type=["pdf"])
